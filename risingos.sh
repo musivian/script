8c4fca4867c717a54f5af195e51f234e1261b4e5
Demi <kizura.vian@gmail.com>
2024-12-03 17:48:34 +0800

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -26,7 +26,8 @@ rm -rf vendor/lineage
 git clone https://github.com/musivian/device_xiaomi_sunny.git --depth 1 -b lineage-22 device/xiaomi/sunny
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fifteen device/qcom/common
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b vauxite device/qcom/qssi
-git clone https://github.com/musivian/device_xiaomi_sunny-kernel.git --depth 1 -b fifteen device/xiaomi/sunny-kernel
+git clone https://github.com/musivian/kernel_xiaomi_sunny.git --depth 1 -b staging device/xiaomi/sunny-kernel
+#git clone https://github.com/musivian/device_xiaomi_sunny-kernel.git --depth 1 -b fifteen device/xiaomi/sunny-kernel
 
 # Clone vendor repositories for Xiaomi and Qualcomm dependencies
 #git clone https://github.com/yaap/vendor_xiaomi_sunny.git --depth 1 -b fifteen vendor/xiaomi/sunny
