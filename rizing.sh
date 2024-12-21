62ddac1e56b41c44ea06667734b6afc15e78cfb0
Demi <kizura.vian@gmail.com>
2024-12-21 15:29:58 +0800

    Update rizing.sh

--- rizing.sh
+++ rizing.sh
@@ -23,15 +23,15 @@ rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
 rm -rf vendor/lineage
 
 # Clone the necessary device repositories
-git clone https://github.com/musivian/device_xiaomi_sunny.git --depth 1 -b fifteen device/xiaomi/sunny
+git clone https://github.com/musivian/device_xiaomi_sunny_rising.git --depth 1 -b fifteen device/xiaomi/sunny
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fifteen device/qcom/common
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b vauxite device/qcom/qssi
 
-# Clone inline kernel (NetErnels)
-git clone https://github.com/musivian/kernel_xiaomi_sunny.git --depth 1 -b rising kernel/xiaomi/sunny
+# Clone prebuilt kernel (NetErnels)
+git clone https://github.com/musivian/device_xiaomi_sunny-kernel.git --depth 1 -b fifteen device/xiaomi/sunny-kernel
 
 # Clone vendor repositories for Xiaomi and Qualcomm dependencies
-git clone https://github.com/musivian/vendor_xiaomi_sunny.git --depth 1 -b fifteen vendor/xiaomi/sunny
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sunny --depth 1 -b fifteen vendor/xiaomi/sunny
 git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fifteen vendor/qcom/common
 git clone https://github.com/yaap/vendor_qcom_opensource_core-utils.git --depth 1 -b fifteen vendor/qcom/opensource/core-utils
 
