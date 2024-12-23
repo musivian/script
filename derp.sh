29e136219a59d4b98431341e3adb4fda4ce7827f
Demi <kizura.vian@gmail.com>
2024-12-23 11:30:16 +0800

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -11,7 +11,7 @@ rm -rf device/xiaomi/sunny
 rm -rf device/qcom/common
 rm -rf device/qcom/qssi
 rm -rf vendor/xiaomi/sunny
-rm -rf kernel/xiaomi/sunny
+rm -rf device/xiaomi/sunny-kernel
 rm -rf vendor/qcom/common
 rm -rf vendor/qcom/opensource/core-utils
 rm -rf packages/apps/DisplayFeatures
@@ -23,15 +23,15 @@ rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
 rm -rf vendor/derp
 
 # Clone the necessary device repositories
-git clone https://github.com/musivian/device_xiaomi_sunny.git --depth 1 -b derp device/xiaomi/sunny
+git clone https://github.com/musivian/device_xiaomi_sunny_rising.git --depth 1 -b fifteen device/xiaomi/sunny
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fifteen device/qcom/common
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b vauxite device/qcom/qssi
 
-# Clone inline kernel (NetErnels)
-git clone https://github.com/musivian/kernel_xiaomi_sunny.git --depth 1 -b derp kernel/xiaomi/sunny
+# Clone prebuilt kernel (NetErnels)
+git clone https://github.com/musivian/device_xiaomi_sunny-kernel.git --depth 1 -b fifteen device/xiaomi/sunny-kernel
 
 # Clone vendor repositories for Xiaomi and Qualcomm dependencies
-git clone https://github.com/musivian/vendor_xiaomi_sunny.git --depth 1 -b fifteen vendor/xiaomi/sunny
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sunny --depth 1 -b fifteen vendor/xiaomi/sunny
 git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fifteen vendor/qcom/common
 git clone https://github.com/yaap/vendor_qcom_opensource_core-utils.git --depth 1 -b fifteen vendor/qcom/opensource/core-utils
 
