c2a62cb8621866c939a3696eaabfa6c13416ebfb
Demi <kizura.vian@gmail.com>
2024-12-24 22:59:21 +0800

    Update mist.sh

--- mist.sh
+++ mist.sh
@@ -11,7 +11,7 @@ rm -rf device/xiaomi/sunny
 rm -rf device/qcom/common
 rm -rf device/qcom/qssi
 rm -rf vendor/xiaomi/sunny
-rm -rf kernel/xiaomi/sunny
+rm -rf device/xiaomi/sunny-kernel
 rm -rf vendor/qcom/common
 rm -rf vendor/qcom/opensource/core-utils
 rm -rf packages/apps/DisplayFeatures
@@ -27,8 +27,8 @@ git clone https://github.com/musivian/device_xiaomi_sunny.git --depth 1 -b mist 
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fifteen device/qcom/common
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b vauxite device/qcom/qssi
 
-# Clone inline kernel (NetErnels)
-git clone https://github.com/musivian/kernel_xiaomi_sunny.git --depth 1 -b mist kernel/xiaomi/sunny
+# Clone prebuilt kernel (NetErnels)
+git clone https://github.com/musivian/device_xiaomi_sunny-kernel.git --depth 1 -b mist device/xiaomi/sunny-kernel
 
 # Clone vendor repositories for Xiaomi and Qualcomm dependencies
 git clone https://github.com/musivian/vendor_xiaomi_sunny.git --depth 1 -b fifteen vendor/xiaomi/sunny
